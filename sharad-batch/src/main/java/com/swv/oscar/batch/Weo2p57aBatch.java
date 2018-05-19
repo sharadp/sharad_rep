5bf6f833134be05fadb5cdd04501198d3142e3ff
sharadp <sharad.pratap@gmail.com>
2018-05-19 15:46:45 -0400

    Update Weo2p57aBatch.java

--- sharad-batch/src/main/java/com/swv/oscar/batch/Weo2p57aBatch.java
+++ sharad-batch/src/main/java/com/swv/oscar/batch/Weo2p57aBatch.java
@@ -25,9 +25,9 @@ public class Weo2p57aBatch {
    public void launchBatch() {
 	   
 
-	   System.out.println("111111111111111111111");
+	   System.out.println("xxxxxxxxxxxxxxxxxxxxx");
 
-	   System.out.println("222222222222222222222");
+	   System.out.println("yyyyyyyyyyyyyyyyyyyyyy");
 
 
 
