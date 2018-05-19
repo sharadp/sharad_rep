5e9197731e3eed23b618ce968d329e5960d097fa
sharadp <sharad.pratap@gmail.com>
2018-05-19 15:00:10 -0400

    Update Weo2p57aBatch.java

--- sharad-batch/src/main/java/com/swv/oscar/batch/Weo2p57aBatch.java
+++ sharad-batch/src/main/java/com/swv/oscar/batch/Weo2p57aBatch.java
@@ -25,9 +25,9 @@ public class Weo2p57aBatch {
    public void launchBatch() {
 	   
 
-	   System.out.println("vvvvvvvvvvvvvvvv");
+	   System.out.println("new ching 111111111");
 
-	   System.out.println("wwwwwwwwwwwwwwww");
+	   System.out.println("new ching 222222222");
 
 
 
