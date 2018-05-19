ced9dc11d981d6850c37d54a72f73154474f15a8
sharadp <sharad.pratap@gmail.com>
2018-05-19 09:06:31 -0400

    Update Weo2p57aBatch.java

--- sharad-batch/src/main/java/com/swv/oscar/batch/Weo2p57aBatch.java
+++ sharad-batch/src/main/java/com/swv/oscar/batch/Weo2p57aBatch.java
@@ -25,6 +25,7 @@ public class Weo2p57aBatch {
    public void launchBatch() {
 	   
 	   System.out.println("wdaqdedfeafdf");
+	    System.out.println("bcd");
       ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
       long startTime = new Date().getTime();
       try {
